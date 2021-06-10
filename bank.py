604245f569d76e638cda201fb87b5a9b06f185df
Beldineneema <79199587+Beldineneema@users.noreply.github.com>
2021-06-10 23:40:03 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,33 +1,89 @@
+from datetime import datetime, time
+now=datetime.now()
 class Account:
-    def __init__(self,accountType,pin,name,phoneNumber):
-        self.accountType=accountType
-        self.balance=0 
-        self.pin=pin
+    def __init__(self,name,phone,account,loan_limit) :
         self.name=name
-        self.phoneNumber=phoneNumber
-    def deposit_cash(self,amount):
-        if amount >0:
-          self.balance+=amount
-        # return f"Dear {self.name} you have deposited {amount} and your new balance is {self.balance}"
-          return  f"he amount is greater than zero"
+        self.phone=phone
+        self.account=account
+        self.balance=0
+        self.loan=0
+        self.loan_limit=loan_limit
+        self.transaction=[]
+        self.repay_loan=[]
+        self.borrow=[]
+        self.withdraw=[]
+
+
+    # def show(self,balance):
+    #     return self.balance
+
+    def deposit(self,amount):
+        if amount <=0:
+            return f"the amount must be grater than zero"
+        else:
+            self.balance +=amount
+            transaction={
+                "amount":amount,
+                "balance":self.balance,
+                "time":now.strftime ("%D"),
+                "narration":"Deposit"
+                }
+            self.transaction.append(transaction)
+            return f"dear {self.name} you have deposited KES {amount} your new balance is {self.balance}"
+
+    def withdraw(self,amount):
+        if amount <=0:
+            return "the amount must be grater than zero"
+        elif (amount<self.balance):
+            return "the amount must be less than the balance"
         else:
             self.balance+=amount
-    def show_balance(self):
-        return self.balance
-    def withdraw_cash(self,amount):
-        if amount <=0:
-            return f" The amount must be greater than zero"
-        elif amount > self.balance:
-             return f" The amount must be less than the balance"
+            withdraw={
+                "amount":amount,
+                "balance":self.balance,
+                "time":now.strftime ("%D"),
+                "narration":"Deposit"
+                }
+            self.withdraw.append(withdraw)
+            return f"dear {self.name} you have deposited KES {amount} your new balance is {self.balance}"
+    def borrow(self,amount):
+        if amount<=self.loan_limit:
+            return f"you are above your loan limit"
+        elif self.loan>0:
+            return f"you cant borrow loan"
         else:
-            self.balance-=amount
-            return f"your balance is {self.balance} after withdrawing {amount} " 
-    def borrow(self,loan,loanLimit):
-        # loanLimit=7000
-        if self.balance <=loanLimit:
-            return f"Dear {self.name} of account {self.accountType} and phone {self.phoneNumber} your loan limit is {loanLimit} "
-        elif loan < 0:
-            return f"you qualify for a loan."
+            self.loan+=1
+            self.balance+=amount
+            borrow={
+                "amount":amount,
+                "balance":self.balance,
+                "time":now.strftime ("%D"),
+                "narration":"Deposit"
+                }
+            self.borrow.append(borrow)
+            return f"dear {self.name} you have deposited KES {amount} your new balance is {self.balance}"    
+    def get_statement(self):
+        for transaction in self.transaction:
+            narration =transaction["narration"]
+            amount =transaction["amount"]
+            balance =transaction["balance"]
+            time =transaction["time"]
+            print(f"{time}{narration}{amount} balance {balance}")
+
+    def repay_loan(self,amount):
+        if amount<=0:
+            return f"you are above your loan limit"
+        elif amount<self.loan:
+            self.loan-=amount
+            return f"amount has been reduced"
         else:
-            self.balance+=loan
-            return f"Dear {self.name} of account {self.accountType} and phone {self.phoneNumber} your loan limit is {loanLimit} and you new balance is {self.balance}"
+            extra=amount-self.loan
+            self.balance+=extra
+            repay_loan={
+                "amount":amount,
+                "balance":self.balance,
+                "time":now.strftime ("%D"),
+                "narration":"Deposit"
+                }
+            self.transaction(repay_loan)
+            return f"dear {self.name} you have repay your loan and your balance is {self.balance}"
